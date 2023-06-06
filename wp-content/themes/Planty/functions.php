2071c4285e1973ca22d6c157ed88e689c4a7d95a
Julian Joly <julianjoly1999@hotmail.com>
2023-06-06 10:11:37 +0200

    Etape 4: Header retravaillé en mode responsive et intégration du hook vers la page admin

--- wp-content/themes/Planty/functions.php
+++ wp-content/themes/Planty/functions.php
@@ -1,7 +1,21 @@
 <?php
-
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
-function theme_enqueue_styles()
-{
+function theme_enqueue_styles() {
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
+    if (is_user_logged_in()) {
+        wp_enqueue_style('margintop-style', get_stylesheet_directory_uri() . '/css/margintop.css', array(), filemtime(get_stylesheet_directory() . '/css/margintop.css'));
+    }
+}
+
+add_filter('wp_nav_menu_items', 'add_extra_item_to_nav_menu');
+function add_extra_item_to_nav_menu($items) {
+    if (is_user_logged_in()) {
+        $items = '
+        <li id="menu-item-46" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-46"><a href="'. get_permalink(get_page_by_title('Nous Rencontrez')) .'">Nous rencontrez</a></li>
+        <li id="menu-item-47" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-47"><a href="'. admin_url() .'">Admin</a></li>
+        <li id="menu-item-45" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-45"><a href="'. get_permalink(get_page_by_title('Commander')) .'">Commander</a></li>
+        ';
+    }
+    return $items;
 }
---
+++ wp-content/themes/Planty/header.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * The header.
+ *
+ * This is the template that displays all of the <head> section and everything up until main.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?> <?php twentytwentyone_the_html_classes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content">
+		<?php
+		/* translators: Hidden accessibility text. */
+		esc_html_e( 'Skip to content', 'twentytwentyone' );
+		?>
+	</a>
+
+<?php
+$wrapper_classes  = 'site-header';
+$wrapper_classes .= has_custom_logo() ? ' has-logo' : '';
+$wrapper_classes .= has_nav_menu( 'primary' ) ? ' has-menu' : '';
+?>
+
+<header id="masthead" class="<?php echo esc_attr( $wrapper_classes ); ?>">
+
+	<div class="site-branding">
+		<div class="site-logo"><?php the_custom_logo(); ?></div>
+		<p class="text-logo">energy drink</p>
+	</div><!-- .site-branding -->
+
+	<nav id="site-navigation" class="primary-navigation " aria-label="<?php esc_attr_e( 'Primary menu', 'twentytwentyone' ); ?>">
+		<div class="menu-button-container">
+			<button id="primary-mobile-menu" class="button" aria-controls="primary-menu-list" aria-expanded="false">
+				<span class="dropdown-icon open"><?php esc_html_e( 'Menu', 'twentytwentyone' ); ?>
+					<?php echo twenty_twenty_one_get_icon_svg( 'ui', 'menu' ); // phpcs:ignore WordPress.Security.EscapeOutput ?>
+				</span>
+				<span class="dropdown-icon close"><?php esc_html_e( 'Close', 'twentytwentyone' ); ?>
+					<?php echo twenty_twenty_one_get_icon_svg( 'ui', 'close' ); // phpcs:ignore WordPress.Security.EscapeOutput ?>
+				</span>
+			</button><!-- #primary-mobile-menu -->
+		</div><!-- .menu-button-container -->
+		<?php
+		wp_nav_menu(
+			array(
+				'theme_location'  => 'primary',
+				'menu_class'      => 'menu-wrapper',
+				'container_class' => 'primary-menu-container',
+				'items_wrap'      => '<ul id="primary-menu-list" class="%2$s">%3$s</ul>',
+				'fallback_cb'     => false,
+			)
+		);
+		?>
+	</nav><!-- #site-navigation -->
+
+</header><!-- #masthead -->
+
+	<div id="content" class="site-content">
+		<div id="primary" class="content-area">
+			<main id="main" class="site-main">
